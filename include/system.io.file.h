9eea0e7c82a04cb89d3a169b57584feac2fe0d83
Wouter Saaltink <wouter.saaltink@gmail.com>
2018-04-04 00:14:07 +0200

    Implemented read and write methods of File class

--- include/system.io.file.h
+++ include/system.io.file.h
@@ -65,6 +65,9 @@ public:
 
 using namespace System::IO;
 
+#include <fstream>
+#include <streambuf>
+
 // Appends lines to a file, and then closes the file. If the specified file does not exist, this method creates a file, writes the specified lines to the file, and then closes the file.
 void File::AppendAllLines(std::string const &path, std::vector<std::string> const &lines)
 { }
@@ -78,6 +81,19 @@ std::vector<unsigned char> File::ReadAllBytes(std::string const &path)
 {
     std::vector<unsigned char> result;
 
+    std::ifstream input(path, std::ifstream::binary);
+
+    if (!input.good())
+    {
+        throw std::ios_base::failure("could not open input file for writing");
+    }
+
+    while (input.good())
+    {
+        result.push_back(input.get());
+    }
+    input.close();
+
     return result;
 }
 
@@ -86,28 +102,87 @@ std::vector<std::string> File::ReadAllLines(std::string const &path)
 {
     std::vector<std::string> result;
 
+    std::ifstream input(path);
+
+    if (!input.good())
+    {
+        throw std::ios_base::failure("could not open input file for writing");
+    }
+
+    for(std::string line; std::getline(input, line); )
+    {
+        result.push_back(line);
+    }
+    input.close();
+
     return result;
 }
 
 // Opens a text file, reads all lines of the file, and then closes the file.
 std::string File::ReadAllText(std::string const &path)
 {
-    std::string result;
+    std::ifstream input(path);
+
+    if (!input.good())
+    {
+        throw std::ios_base::failure("could not open input file for writing");
+    }
+
+    std::string result((std::istreambuf_iterator<char>(input)),
+                     std::istreambuf_iterator<char>());
+
+    input.close();
 
     return result;
 }
 
 // Creates a new file, writes the specified byte array to the file, and then closes the file. If the target file already exists, it is overwritten.
 void File::WriteAllBytes(std::string const &path, std::vector<unsigned char> const &bytes)
-{ }
+{
+    std::ofstream output(path, std::ofstream::binary);
+
+    if (!output.good())
+    {
+        throw std::ios_base::failure("could not open output file for writing");
+    }
+
+    output.write((char *)bytes.data(), bytes.size());
+
+    output.close();
+}
 
 // Creates a new file, writes a collection of strings to the file, and then closes the file.
 void File::WriteAllLines(std::string const &path, std::vector<std::string> const &lines)
-{ }
+{
+    std::ofstream output(path);
+
+    if (!output.good())
+    {
+        throw std::ios_base::failure("could not open output file for writing");
+    }
+
+    for (auto line : lines)
+    {
+        output.write(line.data(), line.size());
+        output.write("\n", 1);
+    }
+
+    output.close();
+}
 
 // Creates a new file, writes the specified string to the file, and then closes the file. If the target file already exists, it is overwritten.
 void File::WriteAllText(std::string const &path, std::string const &text)
-{ }
+{
+    std::ofstream output(path);
+
+    if (!output.good())
+    {
+        throw std::ios_base::failure("could not open output file for writing");
+    }
+
+    output.write(text.data(), text.size());
+
+    output.close();}
 
 #ifdef _WIN32
 #include <windows.h>
